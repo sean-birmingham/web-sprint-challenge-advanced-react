5fc03303dc6625eef1f92968f65db6acea735e97
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-03 12:10:41 -0400

    form working with useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue, submitCb) => {
+  const [values, setValues] = useState(initialValue);
+
+  const handleChange = (e) => {
+    e.persist();
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitCb();
+    setValues(initialValue);
+  };
+
+  return [values, handleChange, handleSubmit];
+};
